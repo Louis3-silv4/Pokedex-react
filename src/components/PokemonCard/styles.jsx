f271d2d940aef720f68b1f10175fd9bf95c7bbc7
louise <louise.silva@g.globo>
2023-02-24 18:10:33 -0300

    implementando hover no card

--- src/components/PokemonCard/styles.jsx
+++ src/components/PokemonCard/styles.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-export const Card = styled.div`
+export const StyleCard = styled.div ``;
+
+export const Card = styled(StyleCard)`
  display:flex;
  box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
  background-color: #757579;
@@ -9,16 +11,14 @@ export const Card = styled.div`
  border-top-right-radius: 33px;
  border-bottom-right-radius: 70px;
  color: #fff;
- &hover{
+ &:hover{
   background: rgb(217,217,217);
   background: linear-gradient(124deg, rgba(217,217,217,1) 57%, rgba(50,50,149,1) 57%);  
   transform: scale(1.06);
   color:black;
  }
 `
-export const Img = styled.img`
-  width:100px;
-`
+
 export const Info = styled.div`
   display: flex;
 `
@@ -33,10 +33,10 @@ export const ButtonInfo = styled.div`
   padding: 30px 0 0 130px;
 `
 export const ButtonDetalhar = styled.button`
-  width:55px;
+  width:66px;
   height:20px
 `
 export const ButtonFavoriteCard = styled.button`
-  width:55px;
+  width:66px;
   height:20px
 `
