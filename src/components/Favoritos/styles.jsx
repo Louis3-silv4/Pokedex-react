dd29d09da5ff183d029f7dab6de2a84a04a8eea2
louise <louise.silva@g.globo>
2023-03-23 16:34:55 -0300

    Criando style para mensagem de erro

---
+++ src/components/Favoritos/styles.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MenssagemDeErro = styled.div`
+ position: relative;
+ padding: 0.75rem 1.25rem;
+ margin-bottom: 1rem;
+ border: 1px solid transparent;
+ border-radius: 0.25rem;
+ color: #772b35;
+ background-color: #fadddd;
+ border-color: #f8cfcf;
+`
--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -3,6 +3,7 @@ import { useContext, useEffect,useState } from "react";
 import {getPokemon} from '../../api'
 import PokemonCard from '../PokemonCard/PokemonCard'
 import { SearchContext } from '../../context/SearchContext';
+import {MenssagemDeErro} from "../Favoritos/styles"
 
 export default function Pokedex (){
   const [allPokemon,setAllPokemon] = useState([])
@@ -23,7 +24,7 @@ export default function Pokedex (){
  const hasFilteredPokemon = filteredPokemon.length > 0 
  const pokemonsList = hasFilteredPokemon ? filteredPokemon : allPokemon
   
-  console.log({filteredPokemon})
+  console.log({pokemonsList})
   return hasPokemon ? (
     <PokedexContainer>
       {pokemonsList.map((pokemon) => {
@@ -32,5 +33,5 @@ export default function Pokedex (){
           )
       })}
     </PokedexContainer>
-   ) : <p>Lista vazia</p>
+   ) : <MenssagemDeErro>Lista vazia</MenssagemDeErro>
 }
