da097b21b490051d3a48d610621a5f6972cf972d
louise <louise.silva@g.globo>
2023-03-23 16:33:12 -0300

    Exibindo PokemonCard na aba de favoritos

--- src/components/Favoritos/Favoritos.jsx
+++ src/components/Favoritos/Favoritos.jsx
@@ -1,8 +1,22 @@
+import { useContext,useState} from 'react';
+import { PokedexContainer } from "../Pokedex/styles";
+import { FavoriteContext } from "../../context/FavoritesContext";
+import PokemonCard from '../PokemonCard/PokemonCard'
+import {MenssagemDeErro} from "../Favoritos/styles"
+
 export default function Favoritos() {
-  
-  return(
-    <p> Favoritos</p>
-  )
+  const {favoritesPokemon} = useContext(FavoriteContext)
+  const [hasPokemon, setHasPokemon] = useState(favoritesPokemon.length > 0);
+
+return hasPokemon ? (
+  <PokedexContainer>
+    {favoritesPokemon.map((pokemon) => {
+      return (
+        <PokemonCard key={pokemon.id} pokemon={pokemon}/>
+      )
+    })}
+  </PokedexContainer>
+) : <MenssagemDeErro>Não existe pokemon favoritado</MenssagemDeErro>
 
 }
 
