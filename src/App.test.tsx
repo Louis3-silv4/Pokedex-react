73909c222c162f284c3876f721879ea5828c70c0
louise <louisea1356@gmail.com>
2023-07-10 15:48:44 -0300

    cenarios de teste

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,17 @@
 import App from "./App"
 import {render,screen} from "@testing-library/react"
 
-describe('Test jest', () => {
+describe('App', () => {
   it('should works', () => {
     expect(1).toBe(1)
   })
-  it('should render App component', () =>{
+  it('should render component header', () =>{
     render(<App/>)
     expect(screen.getByTestId('container-header')).toBeInTheDocument()
+    //To-do: Como testar o componente <PokedexRoute/>
+  })
+  it('should render component topbar', () =>{
+    render(<App/>)
     expect(screen.getByTestId('container-topbar')).toBeInTheDocument()
     //To-do: Como testar o componente <PokedexRoute/>
   })
