16e9a69312d1d356127614df7386c9c4a947d9bf
louise <louisea1356@gmail.com>
2023-07-10 16:43:26 -0300

    Draft: Criando novos cenários

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -11,13 +11,18 @@ describe("Component|Header", () => {
   })
  it ("should render image", () => {
   render(<Header/>)
-
+  //Primeiro modo
+  const imagePokedex = screen.getByRole('img', {
+    name: /logo pokedex/i
+  })
+  expect(imagePokedex).toBeInTheDocument()
+//Segundo modo
   expect(screen.getByAltText('logo pokedex')).toBeInTheDocument()
  })
  
  it("should render github and linkedin link", () => {
   render(<Header/>)
-  
+  //Primeiro modo
  const linkedinLink = screen.getByAltText("Link para site externo")
  expect(linkedinLink).toBeInTheDocument()
  expect(linkedinLink.getAttribute("href")).toBe("https://www.linkedin.com/in/louise-alves-/ s")
@@ -25,6 +30,13 @@ describe("Component|Header", () => {
  const githubLink = screen.getByAltText("Link para site externo")
  expect(githubLink).toBeInTheDocument()
  expect(githubLink.getAttribute("href")).toBe("https://github.com/Louis3-silv4")
+
+ //Segundo modo
+ const gitHubLinkRole = screen.getByRole('link', { name: /github/i })
+ expect(gitHubLinkRole).toBeInTheDocument()
+
+ const linkedinLinkRole = screen.getByRole('link', { name: /linkedin/i })
+ expect(linkedinLinkRole).toBeInTheDocument()
 
  })
 })
