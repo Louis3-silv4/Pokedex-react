3be2e89f794433e359ed93eea75839402e301468
louise <louisea1356@gmail.com>
2023-07-11 16:51:38 -0300

    Mockando fetch globalmente

--- .jest/setup-tests.js
+++ .jest/setup-tests.js
@@ -1,1 +1,13 @@
 require ('@testing-library/jest-dom')
+
+const fakeFetch = jest.fn().mockImplementation(() => {
+  return Promise.resolve({
+    status: 200,
+    json: ()=> {
+      return Promise.resolve({
+        results: []
+      })
+    }
+  })
+})
+global.fetch = fakeFetch
