ba034cf4f0d28e7ca97fa05315b0eae2bdc39de1
louise <louisea1356@gmail.com>
2023-07-14 11:03:14 -0300

    Criando describe do teste

---
+++ src/routes/PokedexRoute.test.tsx
@@ -0,0 +1,8 @@
+describe("Pokedex Route", () => {
+  it.todo("should render the Pokedex component when the route / is activated", () => {
+ 
+  })
+  it.todo("should render the Favoritos component when the route /favoritos is activated", () => {
+ 
+  })
+})
