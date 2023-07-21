adfeb9203e362703a29443da1cdc624bc18796ea
louise <louisea1356@gmail.com>
2023-07-21 11:40:27 -0300

    Adiconando teste do componente TopBar

--- src/components/TopBar/TopBar.test.tsx
+++ src/components/TopBar/TopBar.test.tsx
@@ -1,5 +1,36 @@
-describe("App", () => {
-  it ("should", () => {
- 
+import { render,screen } from "@testing-library/react"
+import TopBar from "./TopBar"
+import { SearchProvider } from "../../context/SearchContext";
+
+describe("Component|TopBar", () => {
+  it('should render component topbar', () => {
+    render(
+     <SearchProvider>
+       <TopBar />
+     </SearchProvider>
+    )
+
+    expect(screen.getByTestId('container-topbar')).toBeInTheDocument()
   })
- })
+
+  it('should render the title in the topbar ', () => {
+    render(
+     <SearchProvider>
+       <TopBar />
+     </SearchProvider>
+    )
+
+    expect(screen.getByText('Escolha seu Pokémon')).toBeInTheDocument();
+  })
+
+  it('should render the button favoritos in the topbar ', () => {
+    render(
+     <SearchProvider>
+       <TopBar />
+     </SearchProvider>
+    )
+
+    expect(screen.getByRole('link',{ name: /Favoritos/i })).toBeInTheDocument();
+    expect(screen.getByRole('img', {name: /icon favorito/i})).toBeInTheDocument();
+  }) 
+})
